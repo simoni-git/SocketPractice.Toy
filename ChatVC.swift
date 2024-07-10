5ceac2169c0a63308a79abe444dd36f05de861aa
시모니(Simoni) <145527459+simoni-git@users.noreply.github.com>
2024-07-10 23:05:10 +0900

    소켓에 관한 코드 추가
    1. 웹소켓을 여는 메서드 (서버 아직 완성안됨)
    2. 전송버튼 클릭시 메세지를 전송
    3. 리시브메서드를 통해 메세지 받기 + 메세지를 받으면 message 변수에 담고 테이블뷰 리로드

--- ChatVC.swift
+++ ChatVC.swift
@@ -15,6 +15,9 @@ class ChatVC: UIViewController {
     @IBOutlet weak var chatTextField: UITextField!
     @IBOutlet weak var sendBtn: UIButton!
     
+    var webSocket: URLSessionWebSocketTask?//🧪
+    var messages: [String] = [] //🧪 메세지를 받아놓을 통
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.dataSource = self
@@ -25,12 +28,61 @@ class ChatVC: UIViewController {
     }
    private func configure() {
        sendBtn.layer.cornerRadius = 25
+       //1단계[웹소켓 열기]
+       openWebsocket()
     
     }
     
+    func openWebsocket() { // 🧪[웹소켓 여는 메서드]
+        let urlString = "아직 미정임"
+            if let url = URL(string: urlString) {
+                var request = URLRequest(url: url)
+                let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
+                 webSocket = session.webSocketTask(with: request)
+                webSocket?.resume()
+            }
+        
+    }
+    
+    func receiveMessage() {//🧪[메세지를 받는 메서드]
+            webSocket?.receive { [weak self] result in
+                switch result {
+                case .success(let message):
+                    switch message {
+                    case .string(let text):
+                        print("Received text: \(text)")
+                        DispatchQueue.main.async {
+                            self?.messages.append(text)
+                            self?.tableView.reloadData()
+                        }
+                    case .data(let data):
+                        print("Received data: \(data)")
+                    @unknown default:
+                        fatalError()
+                    }
+                    // 다음 메시지를 계속 받기 위해 재귀적으로 호출
+                    self?.receiveMessage()
+                case .failure(let error):
+                    print("Failed to receive message: \(error.localizedDescription)")
+                }
+            }
+        }
     
     @IBAction func tapSendBtn(_ sender: UIButton) {
         print("tapSendBtn - Called")
+               guard let message = self.chatTextField.text else { return }
+               
+               webSocket?.send(URLSessionWebSocketTask.Message.string(message)) { [weak self] error in
+                   if let error = error {
+                       print("Failed with Error \(error.localizedDescription)")
+                   } else {
+                       // 메시지 전송 성공 시 텍스트 필드 초기화
+                       print("메세지 전송이 성공적으로 이루어짐")
+                       DispatchQueue.main.async {
+                           self?.chatTextField.text = ""
+                       }
+                   }
+               }
         
     }
     
@@ -79,7 +131,7 @@ class ChatVC: UIViewController {
 
 extension ChatVC: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return self.messages.count //🧪
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -87,6 +139,7 @@ extension ChatVC: UITableViewDataSource, UITableViewDelegate {
            return UITableViewCell()
         }
         cell.subView.layer.cornerRadius = 10
+        cell.textLabel?.text = messages[indexPath.row] // 메시지를 셀에 표시🧪
         return cell
     }
     
@@ -96,3 +149,16 @@ extension ChatVC: UITableViewDataSource, UITableViewDelegate {
     
 }
 
+extension ChatVC: URLSessionWebSocketDelegate {
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+            print("Web socket opened")
+        receiveMessage() // 웹소켓이 열리면 메시지를 받기 시작
+        }
+
+        
+        func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+            print("Web socket closed")
+            
+        }
+   
+}
